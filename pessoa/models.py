17ed8231be0f9df3adc57692af33634656a30df3
pauloricardoprsm <pauloricardoprsm@gmail.com>
2021-09-14 01:31:03 -0300

    Consulta e exibe em /pessoas/

--- pessoa/models.py
+++ pessoa/models.py
@@ -10,5 +10,8 @@ class Pessoa(models.Model):
     nome_completo = models.CharField(max_length=256, null=True, blank=True)
     data_nascimento = models.DateField(null=True)
     ativa = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.nome_completo
     
 
